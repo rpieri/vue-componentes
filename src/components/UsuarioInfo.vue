7b40f64502f72e5d227332eb4fe7155ec3002298
Raphael De Pieri <raphaeldppf@hotmail.com>
2019-08-23 13:38:32 -0300

    r

---
+++ src/components/UsuarioInfo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="componente">
+        <h2>As Informações de Usuário</h2>
+        <p>Vários detalhes...</p>
+        <p>Nome do Usuário: <strong> {{ nome }} </strong> </p>
+        <button @click="reiniciarNome">Reiniciar Nome</button>
+        <button @click="reiniciarFn">Reiniciar Nome (Callback</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        nome: {
+            type: String,
+            // required: true,
+            // default: 'Anonimo'
+            default: function(){
+                return Array(10).fill(0).join(',')
+            }
+        },
+        reiniciarFn: Function
+    },
+    methods: {
+        inverterNome(){
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome(){
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome )
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .componente {
+        flex: 1;
+        background-color: #ec485f;
+        color: #fff;
+    }
+</style>
